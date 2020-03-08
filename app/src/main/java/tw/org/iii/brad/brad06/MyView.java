d26b4c0f6ab6a43f760f8fff116ac48ec0972b57
user <brad@brad.tw>
2020-03-08 15:33:25 +0800

    3

--- app/src/main/java/tw/org/iii/brad/brad06/MyView.java
+++ app/src/main/java/tw/org/iii/brad/brad06/MyView.java
@@ -15,34 +15,46 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class MyView extends View {
-    private LinkedList<HashMap<String, Float>> line;
+    private Paint paint;
+    private LinkedList<LinkedList<HashMap<String, Float>>> lines;
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        line = new LinkedList<>();
+        lines = new LinkedList<>();
+        paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(10);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //Log.v("brad", "onDraw()");
-        Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(10);
-        for (int i = 1; i<line.size(); i++){
-            HashMap<String,Float> p0 = line.get(i-1);
-            HashMap<String,Float> p1 = line.get(i);
-            canvas.drawLine(p0.get("x"), p0.get("y"), p1.get("x"), p1.get("y"),paint);
+
+        for(LinkedList<HashMap<String,Float>> line : lines){
+            for (int i = 1; i<line.size(); i++){
+                HashMap<String,Float> p0 = line.get(i-1);
+                HashMap<String,Float> p1 = line.get(i);
+                canvas.drawLine(p0.get("x"), p0.get("y"), p1.get("x"), p1.get("y"),paint);
+            }
         }
+
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+
+        if (event.getAction() ==  MotionEvent.ACTION_DOWN){
+            LinkedList<HashMap<String,Float>> line = new LinkedList<>();
+            lines.add(line);
+        }
+
         float ex = event.getX(), ey = event.getY();
         HashMap<String,Float> point = new HashMap<>();
         point.put("x", ex); point.put("y", ey);
-        line.add(point);
+        lines.getLast().add(point);
+
         invalidate();   // repaint()
+
         return true;
     }
 }
